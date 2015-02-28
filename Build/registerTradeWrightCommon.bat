5fb8f7405c7f139ddb3a5430931a014e51ad40fe
Richard L King <rlking@aultan.com>
2015-02-28 18:54:12 +0000

    Correct file locations in register/unregister scripts

--- Build/registerTradeWrightCommon.bat
+++ Build/registerTradeWrightCommon.bat
@@ -1,23 +1,23 @@
 :: registers the TradeWright Common dlls
 
-regsvr32 TWUtilities\TWUtilities40.dll
+regsvr32 ..\TWUtilities\TWUtilities40.dll
 
-regsvr32 ExtProps\ExtProps40.dll
+regsvr32 ..\ExtProps\ExtProps40.dll
 
-regsvr32 ExtEvents\ExtEvents40.dll
+regsvr32 ..\ExtEvents\ExtEvents40.dll
 
-regsvr32 BusObjUtils\BusObjUtils4-0.dll
+regsvr32 ..\BusObjUtils\BusObjUtils4-0.dll
 
-regsvr32 TWControls\TWControls40.ocx
+regsvr32 ..\TWControls\TWControls40.ocx
 
-regsvr32 GraphicsUtils\GraphicsUtils40.dll
+regsvr32 ..\GraphicsUtils\GraphicsUtils40.dll
 
-regsvr32 LayeredGraphics\LayeredGraphics40.dll
+regsvr32 ..\LayeredGraphics\LayeredGraphics40.dll
 
-regsvr32 GraphObjUtils\GraphObjUtils40.dll
+regsvr32 ..\GraphObjUtils\GraphObjUtils40.dll
 
-regsvr32 GraphObj\GraphObj40.dll
+regsvr32 ..\GraphObj\GraphObj40.dll
 
-regsvr32 LayeredGraphics\SpriteControlLib\SpriteControlLib.dll
+regsvr32 ..\SampleApps\LayeredGraphicsTest1\SpriteControlLib\SpriteControlLib.dll
 
 
--- Build/unregisterTradeWrightCommon.bat
+++ Build/unregisterTradeWrightCommon.bat
@@ -1,23 +1,23 @@
 :: unregisters the TradeWright Common dlls
 
-regsvr32 TWUtilities\TWUtilities40.dll -u
+regsvr32 ..\TWUtilities\TWUtilities40.dll -u
 
-regsvr32 ExtProps\ExtProps40.dll -u
+regsvr32 ..\ExtProps\ExtProps40.dll -u
 
-regsvr32 ExtEvents\ExtEvents40.dll -u
+regsvr32 ..\ExtEvents\ExtEvents40.dll -u
 
-regsvr32 BusObjUtils\BusObjUtils4-0.dll -u
+regsvr32 ..\BusObjUtils\BusObjUtils4-0.dll -u
 
-regsvr32 TWControls\TWControls40.ocx -u
+regsvr32 ..\TWControls\TWControls40.ocx -u
 
-regsvr32 GraphicsUtils\GraphicsUtils40.dll -u
+regsvr32 ..\GraphicsUtils\GraphicsUtils40.dll -u
 
-regsvr32 LayeredGraphics\LayeredGraphics40.dll -u
+regsvr32 ..\LayeredGraphics\LayeredGraphics40.dll -u
 
-regsvr32 GraphObjUtils\GraphObjUtils40.dll -u
+regsvr32 ..\GraphObjUtils\GraphObjUtils40.dll -u
 
-regsvr32 GraphObj\GraphObj40.dll -u
+regsvr32 ..\GraphObj\GraphObj40.dll -u
 
-regsvr32 LayeredGraphics\SpriteControlLib\SpriteControlLib.dll -u
+regsvr32 ..\SampleApps\LayeredGraphicsTest1\SpriteControlLib\SpriteControlLib.dll -u
 
 
