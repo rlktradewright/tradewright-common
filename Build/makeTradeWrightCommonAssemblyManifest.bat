66bca74b0b0ed6b103c273b09c47620145448c0a
Richard L King <rlking@aultan.com>
2015-06-26 15:48:16 +0100

    Fix another bug in manifest generation scripts

--- Build/makeTradeWrightCommonAssemblyManifest.bat
+++ Build/makeTradeWrightCommonAssemblyManifest.bat
@@ -9,14 +9,17 @@ path %TW-PROJECTS-DRIVE%%TW-PROJECTS-PATH%\Build\Subscripts;%PATH%
 
 call setMyVersion.bat
 
+pushd %TW-PROJECTS-PATH%\Build
 generateManifest /Ass:TradeWright.Common,%VB6-BUILD-MAJOR%.%VB6-BUILD-MINOR%.0.%VB6-BUILD-REVISION%,"TradeWright Common",TradeWrightCommonComponents.txt ^
                  /Out:..\Bin\TradeWright.Common\TradeWright.Common.manifest ^
                  /Inline
 if errorlevel 1 goto :err
 
 echo Manifest generated
+popd
 exit /B
 
 :err
 echo Manifest generation failed
+popd
 exit /B 1
--- Build/makeTradeWrightCommonExternalComponentsAssemblyManifest.bat
+++ Build/makeTradeWrightCommonExternalComponentsAssemblyManifest.bat
@@ -9,14 +9,17 @@ path %TW-PROJECTS-DRIVE%%TW-PROJECTS-PATH%\Build\Subscripts;%PATH%
 
 call setMyVersion.bat
 
+pushd %TW-PROJECTS-PATH%\Build
 generateManifest /Ass:TradeWright.Common.ExternalComponents,%VB6-BUILD-MAJOR%.%VB6-BUILD-MINOR%.0.%VB6-BUILD-REVISION%,"TradeWright Common ExternalComponents",TradeWrightCommonExternalComponents.txt ^
                  /Out:..\Bin\TradeWright.Common.ExternalComponents\TradeWright.Common.ExternalComponents.manifest ^
                  /Inline
 if errorlevel 1 goto :err
 
 echo Manifest generated
+popd
 exit /B
 
 :err
 echo Manifest generation failed
+popd
 exit /B 1
