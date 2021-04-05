9419f0f8ed1a84b491605c8beff6fef7da2cb995
Richard L King <rlking@aultan.com>
2021-04-05 21:23:27 +0100

    Update makeTypelib.bat to work on both x64 and w32

--- Build/makeTypelib.bat
+++ Build/makeTypelib.bat
@@ -1,4 +1,4 @@
-@echo off
+::@echo off
 setlocal
 
 %TW-PROJECTS-DRIVE%
@@ -6,22 +6,29 @@ path %TW-PROJECTS-DRIVE%%TW-PROJECTS-PATH%\Build;%PATH%
 
 set BIN-PATH=%TW-PROJECTS-PATH%\Bin\TradeWright.Common
 
+if not exist "%BIN-PATH%" mkdir "%BIN-PATH%"
+
+set PROGFILES=%ProgramFiles%
+if exist "%ProgramFiles(x86)%" set PROGFILES=%ProgramFiles(x86)%
+
 :: you may need to edit the following to locate your copy of midl.exe. 
 :: It's for Visual Studio 2008 installed in the default location
-path C:\Program Files\Microsoft SDKs\Windows\v6.0A\bin;%PATH%
-path C:\Program Files\Microsoft Visual Studio 9.0\VC\VCPackages;%PATH%
-path C:\WINDOWS\Microsoft.NET\Framework\v2.0.50727;%PATH%
-path C:\WINDOWS\Microsoft.NET\Framework\v3.5;%PATH%
-path C:\Program Files\Microsoft Visual Studio 9.0\Common7\Tools;%PATH%
-path C:\Program Files\Microsoft Visual Studio 9.0\VC\BIN;%PATH%
-path C:\Program Files\Microsoft Visual Studio 9.0\Common7\IDE;%PATH%
+path %ProgramFiles%\Microsoft SDKs\Windows\v6.0A\bin;%PATH%
+path %PROGFILES%\Microsoft Visual Studio 9.0\VC\VCPackages;%PATH%
+path %SystemRoot%\Microsoft.NET\Framework\v2.0.50727;%PATH%
+path %SystemRoot%\Microsoft.NET\Framework\v3.5;%PATH%
+path %PROGFILES%\Microsoft Visual Studio 9.0\Common7\Tools;%PATH%
+path %PROGFILES%\Microsoft Visual Studio 9.0\VC\BIN;%PATH%
+path %PROGFILES%\Microsoft Visual Studio 9.0\Common7\IDE;%PATH%
 
-set INCLUDE="C:\Program Files\Microsoft Visual Studio 9.0\VC\ATLMFC\INCLUDE;C:\Program Files\Microsoft Visual Studio 9.0\VC\INCLUDE;C:\Program Files\Microsoft SDKs\Windows\v6.0A\include;"
-set "WindowsSdkDir=C:\Program Files\Microsoft SDKs\Windows\v6.0A\"
+set INCLUDE="%PROGFILES%\Microsoft Visual Studio 9.0\VC\ATLMFC\INCLUDE;%PROGFILES%\Microsoft Visual Studio 9.0\VC\INCLUDE;%ProgramFiles%\Microsoft SDKs\Windows\v6.0A\include;"
+set WindowsSdkDir=%ProgramFiles%\Microsoft SDKs\Windows\v6.0A\
 
 :: we have to pushd into typelib to ensure midl
 :: picks up the copy of oaidl.idl that's in there
 pushd %TW-PROJECTS-PATH%\src\typelib
+
+dir
 
 @echo on
 midl /mktyplib203 TWWin32API.idl /out %BIN-PATH%
